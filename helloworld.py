aa5f14ddc98ebc03364103a2dd4659ad74e1fe6a
Demian <mipark72@gmail.com>
2024-06-03 19:09:21 -0700

    freshman grapesmango

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,7 @@ print("this is first github editor input")
 print("this is another pycharm editor input")
 print("push & pull exercise")
 print("What about this ?")
-print("orange")
+print("grapes")
+print("mango")
 
 
