972a3db414d5440b19ca85f1c919c38e281ab55f
Demian <mipark72@gmail.com>
2024-06-03 18:59:39 -0700

    master mango

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,10 @@ print("this is first github editor input")
 print("this is another pycharm editor input")
 print("push & pull exercise")
 print("What about this ?")
+<<<<<<< HEAD
 print("mango")
+=======
+print("orange")
+>>>>>>> freshman
 
 
